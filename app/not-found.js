f80b858c5d3e73b6b80481a428178abfbd467e7c
PalaciosCaroline <caroline2807@yahoo.fr>
2025-02-06 15:18:42 +0100

    not found

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <div style={{ textAlign: "center", padding: "50px" }}>
+        <h1>404 - Page Introuvable</h1>
+        <p>Oups, cette page n&apos;existe pas.</p>
+      </div>
+    );
+  }
